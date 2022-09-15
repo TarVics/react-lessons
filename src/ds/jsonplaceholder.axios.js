d15d65a061f35a11fbe7e074ca07fad19598ef37
TarVics <tarvics@gmail.com>
2022-09-16 01:44:30 +0300

    Lesson 3

--- src/ds/jsonplaceholder.axios.js
+++ src/ds/jsonplaceholder.axios.js
@@ -5,7 +5,7 @@ const _url = 'https://jsonplaceholder.typicode.com/';
 const instance = axios.create({baseURL: _url});
 
 const getUsers = () => instance.get('users');
-const getUser = (id) => instance.get(id.toString());
+const getUser = (id) => instance.get('users/' + id);
 const getUserPosts = (id) => instance.get('users/' + id + '/posts');
 
 export {getUsers, getUser, getUserPosts}
