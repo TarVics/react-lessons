04683b2785444bb23ea86e10b9d5218e7a9ac72c
TarVics <tarvics@gmail.com>
2022-09-19 17:04:29 +0300

    Lesson 2

--- src/components/DataCard/DataRow.js
+++ src/components/DataCard/DataRow.js
@@ -1,7 +1,10 @@
 function DataRow({header, caption, children}) {
     return (
         <>
-            {header && <div className="layout-header">{header}</div>}
+            {header &&
+            <div className="layout-header">
+                <div className="layout-value">{header || '\u00A0'}</div>
+            </div>}
             <div className="layout-row">
                 {caption && <div className="layout-caption">{caption}</div>}
                 <div className="layout-value">{children}</div>
